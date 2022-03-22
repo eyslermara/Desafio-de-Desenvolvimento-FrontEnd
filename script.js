b56fe0052e3793ea37729f9d652ee357421f3ee9
Eysler Mara <eyslermara@hotmail.com>
2022-03-22 03:28:53 -0300

    js

---
+++ script.js
@@ -0,0 +1,24 @@
+const number_element1 = document.getElementById('total1');
+const number_element2 = document.getElementById('total2');
+const number_element3 = document.getElementById('total3');
+
+function mais(){
+    var atual = document.getElementById('total1').value;
+    var novo = atual - (-1);
+    document.getElementById('total1').value = novo;
+}
+function menos(){
+    var atual = document.getElementById('total1').value;
+    if(atual > 0) {
+        var novo = atual - 1;
+        document.getElementById('total1').value = novo;
+    }
+}
+console.log
+
+
+
+
+
+
+
